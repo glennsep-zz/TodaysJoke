965b97625e2085a5c0c1d194f88af5204cb6a775
Glenn Seplowitz <glennsep@optonline.net>
2015-12-20 14:25:45 -0500

    Added toolbar for action button (to send e-mail).  Also places constraints in Add Joke modal view.  Saving to source code before implementing UIPickerView.

--- TodaysJoke/TJKDetailJokeViewController.m
+++ TodaysJoke/TJKDetailJokeViewController.m
@@ -12,10 +12,24 @@
 @property (weak, nonatomic) IBOutlet UITextField *jokeTitle;
 @property (weak, nonatomic) IBOutlet UITextField *jokeCategory;
 @property (weak, nonatomic) IBOutlet UITextView *joke;
-@property (weak, nonatomic) IBOutlet UILabel *jokeCreated;
 
 @end
 
 @implementation TJKDetailJokeViewController
+
+-(void)viewDidLoad
+{
+    UIColor *borderColor = [UIColor colorWithRed:0.76 green:0.76 blue:0.76 alpha:1.0];
+
+    self.joke.layer.borderWidth = 1.0f;
+    self.joke.layer.borderColor = borderColor.CGColor;
+    self.joke.layer.cornerRadius = 5.0;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
+}
 
 @end
