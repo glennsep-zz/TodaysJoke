363a8910607675e3c662859c4f631703b04612a2
Glenn Seplowitz <glennsep@optonline.net>
2016-01-03 15:39:09 -0500

    Added a hard coded joke in main screen to start getting look and feel.

--- TodaysJoke/TJKCenterViewController.m
+++ TodaysJoke/TJKCenterViewController.m
@@ -9,6 +9,11 @@
 #import "TJKCenterViewController.h"
 
 @interface TJKCenterViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *jokeTitle;
+@property (weak, nonatomic) IBOutlet UILabel *jokeCategory;
+@property (weak, nonatomic) IBOutlet UILabel *jokeSubmitted;
+@property (weak, nonatomic) IBOutlet UITextView *joke;
+
 
 @end
 
@@ -21,6 +26,19 @@
     
     // initialize property values
     self.leftButton = 1;
+   
+    // display today's date
+    NSDateFormatter *todaysDate = [[NSDateFormatter alloc] init];
+    [todaysDate setDateStyle:NSDateFormatterFullStyle];
+    
+    NSDate *now = [[NSDate alloc] init];
+    
+    NSString *theDate = [todaysDate stringFromDate:now];
+    NSString *jokeTitleString = [@"Joke of the day for " stringByAppendingString:theDate];
+    self.jokeTitle.text = jokeTitleString;
+    [self.jokeTitle setTextColor:[[UIColor alloc] initWithRed:84/256.0 green:174/256.0 blue:166/256.0 alpha:1]];
+    [self.jokeTitle setFont:[UIFont systemFontOfSize:17]];
+    self.jokeTitle.textAlignment = NSTextAlignmentCenter;
 }
 
 - (void)didReceiveMemoryWarning {
